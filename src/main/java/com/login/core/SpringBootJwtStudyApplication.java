4a299a6391cf61aa51c939630bfd9573b2af30fe
HwangHarim <scopan01@naver.com>
2023-04-18 14:29:32 +0900

    :tada: init: jwt login project init

---
+++ src/main/java/com/login/core/SpringBootJwtStudyApplication.java
@@ -0,0 +1,13 @@
+package com.login.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtStudyApplication.class, args);
+    }
+
+}
